174a819aa90f18aa867332f9fddd9af07eef87a9
tahacan444 <64565958+tahacan444@users.noreply.github.com>
2021-04-29 11:28:10 +0300

    Update Main.java

--- odev2/src/odev2/Main.java
+++ odev2/src/odev2/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Instructor instructor = new Instructor(1, "Engin", "Demiroğ");
+		Instructor instructor = new Instructor(1, "Engin", "DemiroÄŸ");
 
-		Course course1 = new Course(1, "Engin Demiroğ", 10, "Java");
-		Course course2 = new Course(2, "Engin Demiroğ", 20, "C#");
-		Course course3 = new Course(3,"Engin Demiroğ",30,"Python");
+		Course course1 = new Course(1, "Engin DemiroÄŸ", 10, "Java");
+		Course course2 = new Course(2, "Engin DemiroÄŸ", 20, "C#");
+		Course course3 = new Course(3,"Engin DemiroÄŸ",30,"Python");
 		
 		
 		
